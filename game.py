346469a1989c9443d8e6183f563f36863c59f37a
kevinjunsang <m263636@usna.edu>
2023-11-08 09:12:45 -0500

    Added most of the assets

--- game.py
+++ game.py
@@ -6,31 +6,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-screen.fill(BACKGROUND_BOARD)
 
-clock = pygame.time.Clock()
-
-
-def draw_board():
-    pygame.draw.rect(screen, BLACK_BOARD, (TILE_SIZE, TILE_SIZE,
-                                           SCREEN_WIDTH - 2 * TILE_SIZE, SCREEN_HEIGHT - 2 * TILE_SIZE))
-    for i in range(4):
-        for j in range(4):
-            pygame.draw.rect(screen, WHITE_BOARD, (TILE_SIZE + 2 * i * TILE_SIZE,
-                                                   TILE_SIZE + 2 * j * TILE_SIZE,
-                                                   TILE_SIZE,
-                                                   TILE_SIZE))
-    for i in range(4):
-        for j in range(4):
-            pygame.draw.rect(screen, WHITE_BOARD, (2 * TILE_SIZE + 2 * i * TILE_SIZE,
-                                                   2 * TILE_SIZE + 2 * j * TILE_SIZE,
-                                                   TILE_SIZE,
-                                                   TILE_SIZE))
-    pygame.display.flip()
-
-
-draw_board()
-print(BOARD)
 
 while True:
     # listen for events
